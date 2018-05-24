27ea2bd0738566ea36956f0893c5711121347e09
Andy Joshowitz <ajoshow@sas.upenn.edu>
2018-05-24 14:06:25 -0400

    instructor table migration

---
+++ db/migrate/20180524180558_add_instructor_id_to_courses.rb
@@ -0,0 +1,4 @@
+class AddInstructorIdToCourses < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_24_174118) do
+ActiveRecord::Schema.define(version: 2018_05_24_180558) do
 
   create_table "courses", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2018_05_24_174118) do
     t.integer "instructor"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_courses_on_user_id"
   end
 
   create_table "instructors", force: :cascade do |t|
