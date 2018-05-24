6220de9aa11459ac702384b520f0cf684edc4c5e
Andy Joshowitz <ajoshow@sas.upenn.edu>
2018-05-24 13:15:16 -0400

    added course resource

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -1,6 +1,3 @@
 class CourseSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :department, :course_number
-  belongs_to :user
-  belongs_to :instructor
-  has_many :reviews
+  attributes :id
 end
