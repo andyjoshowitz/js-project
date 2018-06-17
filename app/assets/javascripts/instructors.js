b8c502413ba5da472b827750dfd1250235dba1db
Andy Joshowitz <ajoshow@sas.upenn.edu>
2018-06-17 00:38:26 -0400

    fixed instructor show

--- app/assets/javascripts/instructors.js
+++ app/assets/javascripts/instructors.js
@@ -4,8 +4,13 @@ function loadInstructor(data) {
       $(".second-name").text(data["second_name"]);
       $(".js-next").attr("data-id", data["id"]);
       $(".js-previous").attr("data-id",data["id"]);
-
+      //$(".instructor-courses").empty();
+      data["course_list"].forEach(function(element){
+        var course = new Course(element);
+        course.renderDisplay();
+      });
   }
+
 
   $(".js-next").on("click", function(event) {
     var id = $(".js-next").attr("data-id")
@@ -25,3 +30,24 @@ function loadInstructor(data) {
     event.preventDefault();
 
   });
+
+  function Course(data) {
+    this.id = data.id;
+    this.instructor = data.instructor;
+    this.department = data.department;
+    this.title = data.title;
+    this.course_number = data.course_number;
+    this.description = data.description;
+    this.user = data.user;
+  }
+
+
+
+  Course.prototype.renderDisplay = function() {
+    var html = "" ;
+    html += '<ol>' +'<li>' + '<h3 class="instructorCourseDepartment">' + this.department + "-" + this.course_number + ":" + this.title + '</h3>';
+    html += '<p class="card-text">' + this.description + '</p>';
+    html += '</li>';
+    html += '</ol>'
+    $("#instructor-courses").append(html);
+  }
--- app/serializers/instructor_serializer.rb
+++ app/serializers/instructor_serializer.rb
@@ -1,18 +1,22 @@
 class InstructorSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :second_name
+  attributes :id, :first_name, :second_name, :course_list
   has_many :courses
 
-  # def course_list
-  #   object.courses.map do |course|
-  #     {
-  #       id: course.id,
-  #       instructor: {
-  #         id: course.instructor_id,
-  #       },
-  #       content: course.description
-  #       title: course.title
-  #       department: course.department
-  #     }
-  #   end
-  # end
+  def course_list
+    object.courses.map do |course|
+      {
+        id: course.id,
+        instructor: {
+          id: course.instructor_id
+        },
+        user: {
+          id: course.user_id
+        },
+        description: course.description,
+        title: course.title,
+        department: course.department,
+        course_number: course.course_number
+      }
+    end
+  end
 end
