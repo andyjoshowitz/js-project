5e67e71207e10a9663669fa66f57c4a9b3610283
Andy Joshowitz <ajoshow@sas.upenn.edu>
2018-06-17 19:23:51 -0400

    commented out logs

--- app/assets/javascripts/instructors.js
+++ app/assets/javascripts/instructors.js
@@ -15,7 +15,7 @@ function loadInstructor(data) {
   $(".js-next").on("click", function(event) {
     var id = $(".js-next").attr("data-id")
     $.get("/instructors/" + id + "/next", function(data) {
-      console.log(data)
+      //console.log(data)
       loadInstructor(data);
     });
     event.preventDefault();
@@ -24,7 +24,7 @@ function loadInstructor(data) {
   $(".js-previous").on("click", function(event) {
     var id = $(".js-previous").attr("data-id")
     $.get("/instructors/" + id + "/previous", function(data) {
-      console.log(data)
+      //console.log(data)
       loadInstructor(data);
     });
     event.preventDefault();
