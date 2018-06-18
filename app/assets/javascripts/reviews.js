628b4c2e841a1f5a9d0f5ad55ab2c894931ef492
Andy Joshowitz <ajoshow@sas.upenn.edu>
2018-06-18 14:20:38 -0400

    comment out

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -10,10 +10,10 @@ function getReviews(){
       data: $(this).serialize(),
       dataType: "json",
       success: function(json){
-        /*console.log(json.reviews)
-        $("div.reviews ol")
-        // orderedList.html("")
-        console.log(orderedList.html(""))*/
+        // console.log(json.reviews)
+        // $("div.reviews ol")
+        // // orderedList.html("")
+        // console.log(orderedList.html(""))
         json.reviews.forEach(function(review){
           $("#test").append(`<li><a class="review_link" href="http://localhost:3000/courses/${json.id}/reviews/${review.id}">${review.comment.substring(0,20) + "..."}</li>`);
         })
@@ -21,55 +21,3 @@ function getReviews(){
     })
   })
 };
-
-function Review(attributes) {
-  this.id = attributes.id;
-  this.comment = attributes.comment;
-  this.overall_quality = attributes.overall_quality
-  this.average_difficulty = attributes.average_difficulty
-  this.work_amount = attributes.work_amount
-  this.amount_learned = attributes.amount_learned
-  this.instructor_quality = attributes.course
-  this.course = attributes.course;
-  this.user = attributes.user;
-  this.created_at = attributes.created_at;
-}
-
-
-
-Review.prototype.renderReview = function() {
-  var html = "";
-  html += '<li>' + '<a class="review_link" href="http://localhost:3000/courses/${this.course.id}/reviews/${this.id}">' + this.comment.substring(0,20) + "..." + '</li>';
-  $("#test").append(html);
-}
-
-function createReview() {
-  $("form#new_review").on("submit", function(event) {
-    event.preventDefault();
-    var $form = $(this);
-    var url = this.action + "." + "json";
-    var data = $form.serialize();
-    debugger
-    $.ajax({
-      method: "POST",
-      url: url,
-      data: data,
-      success: function(data) {
-        $("#test").val("");
-        var review = new Review(data);
-        review.renderReview();
-      }
-    })
-  })
-}
-
-
-//"http://localhost:3000/courses/4/reviews.json"
-
-
-$(document).ready(function() {
-  getReviews();
-  createReview();
-  // createProtoReview();
-//  showReview();
-});
